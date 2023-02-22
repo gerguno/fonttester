b39943f9cec6a6e8f498e02891fc6fbe526f9617
Oleś Gergun <olesgergun@Oless-MBP.fritz.box>
2023-02-22 14:18:16 +0100

    custom typeface upload nad treatment done

--- src/fontTester.js
+++ src/fontTester.js
@@ -15,7 +15,10 @@ typefaces.map((t) => {
 })
 
 export default function FontTester() {
-  const [typeface, setTypeface] = useState(typefaces[0]);
+  const [typeface, setTypeface] = useState(typefaces[0])
+  const [customTypeface, setCustomTypeface] = useState({})
+  const [customTypefacePopulated, setCustomTypefacePopulated] = useState(false)
+  const [textStyle, setTextStyle] = useState({})
   const [style, setStyle] = useState(typeface.fonts[0].fontTitle.slice(typeface.fonts[0].fontTitle.indexOf(' ') + 1))
   const [showFontStyles, setShowFontStyles] = useState(false)
   const [fontSize, setFontSize] = useState(72);
@@ -31,7 +34,7 @@ export default function FontTester() {
 
   const handleTypefaceChange = (event) => {
     const foundT = typefaces.find(foundT => foundT.title === event.target.value);
-    setTypeface(foundT);
+    setTypeface(foundT)
     setSelectedFeatures([])
     let inputs = document.querySelectorAll('.otf-checkbox');
     for (let i = 0; i < inputs.length; i++) {
@@ -85,15 +88,27 @@ export default function FontTester() {
     }
   }
 
-  const textStyle = {
-    fontFamily: `${typeface.title} ${style}`,
-    fontSize: `${fontSize}px`,
-    letterSpacing: `${letterSpacing}em`,
-    lineHeight: lineHeight,
-    textTransform: `${uppercase ? `uppercase` : `none`}`,
-    textAlign: `${alignment}`,
-    fontFeatureSettings: selectedFeatures.map((s) => `"${s}"`).join(', ')
+  const handleFileChange = (event) => {
+    setCustomTypeface({})
+    setCustomTypefacePopulated(false)
+    setSelectedFeatures([])
+
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = function() {
+      const base64 = btoa(reader.result);
+      setCustomTypeface({base64: base64})
+    };
+    reader.readAsBinaryString(file);
   }
+
+  const handleRemoveCustomTypeface = () => {
+    setCustomTypeface({})
+    setCustomTypefacePopulated(false)
+    setSelectedFeatures([])
+    document.getElementById("drag-n-drop").value = "";
+  }
+
 
   useEffect(() => {
     // Add font features data from base64s to typefaces array 
@@ -104,38 +119,36 @@ export default function FontTester() {
       async function parse() {
         const font = await opentype.parse(Base64Binary.decodeArrayBuffer(typefaces[i].fonts[0].base64))
         const features = [...Array.from(new Set(font.tables.gsub.features.map((f) => f.tag)))].map(tag => ({tag}))
-        
+
         // Names 256-32767
         let fontNames = []
         let counter = 0
 
         if (font.names.hasOwnProperty('256')) {
-
-        for (let key in font.names) {
-            if (/^\d+$/.test(key)) {
-              for (let n in font.names[key]) {
-                fontNames.push(font.names[key][n])
+          for (let key in font.names) {
+              if (/^\d+$/.test(key)) {
+                for (let n in font.names[key]) {
+                  fontNames.push(font.names[key][n])
+                }
               }
             }
           }
-        }
-
-        if (fontNames.length > 0) {
-          for (let i=0; i < features.length; i++) {
-            if (features[i].tag.includes('ss')) {
-              features[i]['name'] = fontNames[counter]
-              counter++
+          if (fontNames.length > 0) {
+            for (let i=0; i < features.length; i++) {
+              if (features[i].tag.includes('ss')) {
+                features[i]['name'] = fontNames[counter]
+                counter++
+              }
             }
           }
-        }
-
-        typefaces[i]['opentypeFeatures'] = features
+          typefaces[i]['opentypeFeatures'] = features
+          return null
       }
 
       parse()
     }
 
-    console.log(typefaces)
+    // console.log(typefaces)
   }, []);
 
   useEffect(() => {
@@ -183,11 +196,146 @@ export default function FontTester() {
       document.removeEventListener('mouseleave', handleMouseLeave);
       document.removeEventListener('click', handleMouseMove);
     };
+    setShowControls(true);
   }, [mouseOverControls]);
 
   useEffect(() => {
-    console.log(selectedFeatures)
-  }, [selectedFeatures])
+    if (customTypeface.base64 && !customTypefacePopulated) {
+      async function parse() {
+        const font = await opentype.parse(Base64Binary.decodeArrayBuffer(customTypeface.base64))
+        const features = [...Array.from(new Set(font.tables.gsub.features.map((f) => f.tag)))].map(tag => ({tag}))
+        
+        // Names 256-32767
+        let fontNames = []
+        let counter = 0
+        let typefaceTitle
+        
+        for (let n in font.names.fullName) {
+          typefaceTitle = font.names.fullName[n]
+        }
+        
+  
+        if (font.names.hasOwnProperty('256')) {
+          for (let key in font.names) {
+              if (/^\d+$/.test(key)) {
+                for (let n in font.names[key]) {
+                  fontNames.push(font.names[key][n])
+                }
+              }
+            }
+          }
+          if (fontNames.length > 0) {
+            for (let i=0; i < features.length; i++) {
+              if (features[i].tag.includes('ss')) {
+                features[i]['name'] = fontNames[counter]
+                counter++
+              }
+            }
+          }
+          setCustomTypeface({
+            ...customTypeface,
+            opentypeFeatures: features,
+            typefaceTitle: typefaceTitle
+          })
+
+          setCustomTypefacePopulated(true)
+
+          return null
+      }
+      parse()
+    }
+  }, [customTypeface])
+
+  useEffect(() => {
+    setTextStyle({
+      fontFamily: (!customTypefacePopulated) ? `${typeface.title} ${style}` : `${customTypeface.typefaceTitle}`,
+      fontSize: `${fontSize}px`,
+      letterSpacing: `${letterSpacing}em`,
+      lineHeight: lineHeight,
+      textTransform: `${uppercase ? `uppercase` : `none`}`,
+      textAlign: `${alignment}`,
+      fontFeatureSettings: selectedFeatures.map((s) => `"${s}"`).join(', ')
+    })
+  }, [typeface, customTypeface, customTypefacePopulated, style, fontSize, letterSpacing, lineHeight, uppercase, alignment, verticalCenter, selectedFeatures])
+
+  useEffect(() => {
+    // Load customTypeface
+    const fontFace = new FontFace(`${customTypeface.typefaceTitle}`, `url(data:application/octet-stream;base64,${customTypeface.base64})`);
+    document.fonts.add(fontFace);
+  }, [customTypefacePopulated])
+
+  const universalControls = (
+    <div id="universal-controls"> 
+    <div>
+        <label>
+        size
+        <input
+            type="range"
+            min="10"
+            max="800"
+            value={fontSize}
+            onChange={handleFontSizeChange}
+        />
+        </label>
+        <span>{fontSize}</span>
+    </div>
+    <div>
+        <label>
+        spacing
+        <input
+            type="range"
+            min="-0.1"
+            max="0.1"
+            step="0.01"
+            value={letterSpacing}
+            onChange={handleLetterSpacingChange}
+        />
+        </label>
+        <span>{Math.floor(letterSpacing*100)}</span>
+    </div>
+    <div>
+        <label>
+        leading
+        <input
+            type="range"
+            min="0.7"
+            max="1.7"
+            step="0.01"
+            value={lineHeight}
+            onChange={handleLineHeightChange}
+        />
+        </label>
+        <span>{lineHeight}</span>
+    </div>
+    <div>
+      <a onClick={handleUppercaseChange}>
+        {uppercase ? 'AA' : 'Aa' }
+      </a>
+    </div>
+    <div>
+      <button
+        value="left"
+        onClick={handleAlignment}
+        style={{ backgroundColor: alignment === 'left' ? 'pink' : 'grey' }}
+      >
+        Left
+        {/* add image through background image */}
+      </button>
+      <button
+        value="center"
+        onClick={handleAlignment}
+        style={{ backgroundColor: alignment === 'center' ? 'pink' : 'grey' }}
+      >
+        Center
+        {/* add image through background image */}
+      </button>
+    </div>
+    <div>
+      <a onClick={handleVerticalCenter}>
+        {verticalCenter ? '[•]' : '[⌜]' }
+      </a>
+    </div>
+  </div>)
 
   return (
     <div>
@@ -206,115 +354,83 @@ export default function FontTester() {
             padding: '16px',
         }}>
             <div>
-                <label>
-                Font family:
-                <select value={typeface.title} onChange={handleTypefaceChange}>
-                    {typefaces.map((t, id) => (
-                        <option key={id} value={t.title}>{t.title}</option>
-                    ))}
-                </select>
-                </label>
+              <input id="drag-n-drop" type="file" accept=".ttf,.otf,.woff" onChange={handleFileChange} />
+              {customTypefacePopulated && 
+                <a onClick={handleRemoveCustomTypeface}>
+                    Remove
+                </a>
+              }
             </div>
             {
-                showFontStyles ? (
+              !customTypefacePopulated 
+                ? 
+                  (
+                    <div id="ktf-fonts">
                     <div>
                         <label>
-                        Font styles:
-                        <select value={style} onChange={handleStyleChange}>
-                            {typeface.fonts.map((f, id) => (
-                                <option key={id} value={`${f.fontTitle.slice(f.fontTitle.indexOf(' ') + 1)}`}>{f.fontTitle.slice(f.fontTitle.indexOf(' ') + 1)}</option>
+                        Font family:
+                        <select value={typeface.title} onChange={handleTypefaceChange}>
+                            {typefaces.map((t, id) => (
+                                <option key={id} value={t.title}>{t.title}</option>
                             ))}
                         </select>
                         </label>
                     </div>
-                ) : <></>
+                    {
+                      showFontStyles ? (
+                          <div>
+                              <label>
+                              Font styles:
+                              <select value={style} onChange={handleStyleChange}>
+                                  {typeface.fonts.map((f, id) => (
+                                      <option key={id} value={`${f.fontTitle.slice(f.fontTitle.indexOf(' ') + 1)}`}>{f.fontTitle.slice(f.fontTitle.indexOf(' ') + 1)}</option>
+                                  ))}
+                              </select>
+                              </label>
+                          </div>
+                      ) : <></>
+                    }
+                    {universalControls}
+                    {
+                      typeface.opentypeFeatures ? (
+                        <div>
+                          OpenType features:
+                          <br/>
+                            {typeface.opentypeFeatures.map((opf, id) => (
+                              <label key={id}>
+                                <input type="checkbox" className="otf-checkbox" value={opf.tag} onChange={handleCheckboxChange}/>
+                                {opf.tag} {(opf.name) && opf.name} <br/>
+                              </label>
+                            ))}
+                        </div>
+                      ) : <></> 
+                    }
+                  </div>      
+                  )
+                :
+                  (
+                    <div id="custom-font">
+                      {universalControls}
+                      {
+                        customTypeface.opentypeFeatures ? (
+                          <div>
+                            OpenType features:
+                            <br/>
+                              {customTypeface.opentypeFeatures.map((opf, id) => (
+                                <label key={id}>
+                                  <input type="checkbox" className="otf-checkbox" value={opf.tag} onChange={handleCheckboxChange}/>
+                                  {opf.tag} {(opf.name) && opf.name} <br/>
+                                </label>
+                              ))}
+                          </div>
+                        ) : <></> 
+                      }
+                    </div>      
+                  )
             }
-            <div>
-                <label>
-                size
-                <input
-                    type="range"
-                    min="10"
-                    max="800"
-                    value={fontSize}
-                    onChange={handleFontSizeChange}
-                />
-                </label>
-                <span>{fontSize}</span>
-            </div>
-            <div>
-                <label>
-                spacing
-                <input
-                    type="range"
-                    min="-0.1"
-                    max="0.1"
-                    step="0.01"
-                    value={letterSpacing}
-                    onChange={handleLetterSpacingChange}
-                />
-                </label>
-                <span>{Math.floor(letterSpacing*100)}</span>
-            </div>
-            <div>
-                <label>
-                leading
-                <input
-                    type="range"
-                    min="0.7"
-                    max="1.7"
-                    step="0.01"
-                    value={lineHeight}
-                    onChange={handleLineHeightChange}
-                />
-                </label>
-                <span>{lineHeight}</span>
-            </div>
-            <div>
-              <a onClick={handleUppercaseChange}>
-                {uppercase ? 'AA' : 'Aa' }
-              </a>
-            </div>
-            <div>
-              <button
-                value="left"
-                onClick={handleAlignment}
-                style={{ backgroundColor: alignment === 'left' ? 'pink' : 'grey' }}
-              >
-                Left
-                {/* add image through background image */}
-              </button>
-              <button
-                value="center"
-                onClick={handleAlignment}
-                style={{ backgroundColor: alignment === 'center' ? 'pink' : 'grey' }}
-              >
-                Center
-                {/* add image through background image */}
-              </button>
-            </div>
-            <div>
-              <a onClick={handleVerticalCenter}>
-                {verticalCenter ? '[•]' : '[⌜]' }
-              </a>
-            </div>
-            {
-             typeface.opentypeFeatures ? (
-              <div>
-                OpenType features:
-                <br/>
-                  {typeface.opentypeFeatures.map((opf, id) => (
-                    <label key={id}>
-                      <input type="checkbox" class="otf-checkbox" value={opf.tag} onChange={handleCheckboxChange}/>
-                      {opf.tag} {(opf.name) && opf.name} <br/>
-                    </label>
-                  ))}
-              </div>
-             ) : <></> 
-            }
-
       </div>
-      <div style={{
+
+      <div id="typefield-container" style={{
         display: `${verticalCenter ? `flex` : `block`}`,
         alignItems: `${verticalCenter ? `center` : `initial`}`,
         justifyContent: `${verticalCenter ? `center` : `initial`}`,
@@ -325,6 +441,7 @@ export default function FontTester() {
             over the lazy dog. The quick brown fox jumps over the lazy dog.
         </div>
       </div>
+
     </div>
   );
 }
